35d8e90d25f358f41465e850cad8be0823a39da1
UyuTeko <uyutako2@gmail.com>
2026-03-01 21:51:28 +0900

    constに変更

--- mainLogic.js
+++ mainLogic.js
@@ -1,4 +1,4 @@
-export function main() {
+export const main = function() {
     const canvas = document.querySelector('canvas');
     const ctx = canvas.getContext('2d');
     game._listeners.exitframe.push(() => {
