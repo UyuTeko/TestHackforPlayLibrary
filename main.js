11e6f9bdeead9456a2e04fa8c2ee73fe1ee108b7
UyuTeko <uyutako2@gmail.com>
2026-03-01 19:00:41 +0900

    エキスポートしてみる

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'gray';
-ctx.fillRect(0, 0, 480, 320);
+export function main() {
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'gray';
+    ctx.fillRect(0, 0, 480, 320);
+    game._listeners.exitframe.push(() => {
+        ctx.fillRect(0, 0, 480, 320);
+    });
+}
